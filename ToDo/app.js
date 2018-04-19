6bbd00cb10542bb00d28abf0e6a8c8ce4f2d7fc5
Kostiantyn.Sikhnevych <Kostiantyn.Sikhnevych@infopulse.com>
2018-04-19 09:52:39 +0300

    update( todo app): midleware

--- ToDo/app.js
+++ ToDo/app.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var contr = require('./controller/controller');
-var app = express();
+let express = require('express');
+let contr = require('./controller/controller');
+let app = express();
 app.set('view engine', 'ejs');
-app.use('/assets', express.static('/assets'));
+app.use('/assets', express.static('./assets'));
 app.listen(3000);
 console.log('Listening port 3000');
 contr(app);

925cc32f1555310afd56ba2fc2c95e7e4e8049a1
Kostiantyn.Sikhnevych <Kostiantyn.Sikhnevych@infopulse.com>
2018-04-20 11:50:06 +0300

    update (Tode): clean code

--- ToDo/app.js
+++ ToDo/app.js
@@ -3,6 +3,6 @@ let contr = require('./controller/controller');
 let app = express();
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('./assets'));
-app.listen(3000);
-console.log('Listening port 3000');
+app.listen(3001);
+console.log('Listening port 3001');
 contr(app);
--- ToDo/controller/controller.js
+++ ToDo/controller/controller.js
@@ -4,23 +4,17 @@ let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function (app) {
 
-    app.get("/todo", function (req, res) {
-        res.render("todo", {todos: data});
-    });
-    app.post("/todo", urlencodedParser, function (req, res) {
-       console.log(req.body);
-       data.push(req.body);
-       res.json(data);
-    });
-    app.delete("/todo/:item", function (req, res) {
-        console.log("start delete");
+    app.get("/todo", (req, res) => res.render("todo", {todos: data}));
 
-        data = data.filter(function (value) {
-            console.log(value.item.replace(/ /g, '-'));
-            console.log(req.params.item);
-            return value.item.replace(/ /g, '-') !== req.params.item;
-        });
+    app.post("/todo", urlencodedParser, (req, res) => {
+        console.log(`Add new item ${req.body.item}`);
+        data.push(req.body);
         res.json(data);
-        console.log(data);
     });
-}; 
+
+    app.delete("/todo/:item", (req, res) => {
+        console.log(`Delete item ${req.params.item}`);
+        data = data.filter( (value) => value.item.replace(/ /g, '-') !== req.params.item);
+        res.json(data);
+    });
+};

300cfec8c64af65584afa5d1f68876f61d60070d
Kostiantyn.Sikhnevych <Kostiantyn.Sikhnevych@infopulse.com>
2018-04-20 15:04:50 +0300

    update (Todo): mongo + routes

--- ToDo/controller/controller.js
+++ ToDo/controller/controller.js
@@ -1,20 +1,44 @@
 let bodyParser = require('body-parser');
-let data = [{'item': 'first'},{'item': 'second'}];
-let urlencodedParser = bodyParser.urlencoded({ extended: false });
+let mongoose = require('mongoose');
+//let data = [{'item': 'first'},{'item': 'second'}];
+let urlencodedParser = bodyParser.urlencoded({extended: false});
+
+//conect to database
+
+mongoose.connect('mongodb://admin:admin@ds247499.mlab.com:47499/todo-app-kosi');
+
+let todoSchema = new mongoose.Schema({
+	item: String
+});
+
+let Todo = mongoose.model('Todo', todoSchema);
+/*let itemOhe = Todo({item: 'first'}).save( (err) => {
+    if (err) throw err;
+    console.log('item saved')
+});*/
+
 
 module.exports = function (app) {
 
-    app.get("/todo", (req, res) => res.render("todo", {todos: data}));
+	app.get("/", (req, res) => {
+		Todo.find({}, (err, data) => {
+			if (err) throw err;
+			res.render("todo", {todos: data})
+		});
+	});
 
-    app.post("/todo", urlencodedParser, (req, res) => {
-        console.log(`Add new item ${req.body.item}`);
-        data.push(req.body);
-        res.json(data);
-    });
+	app.post("/", urlencodedParser, (req, res) => {
+		Todo(req.body).save( (err, data) => {
+			if (err) throw err;
+			res.json(data);
+		});
+	});
 
-    app.delete("/todo/:item", (req, res) => {
-        console.log(`Delete item ${req.params.item}`);
-        data = data.filter( (value) => value.item.replace(/ /g, '-') !== req.params.item);
-        res.json(data);
-    });
+	app.delete("/:item", (req, res) => {
+
+		Todo.find({item: req.params.item.replace(/-/g, ' ')}).remove( (err, data) => {
+			if (err) throw err;
+			res.json(data);
+		});
+	});
 };
